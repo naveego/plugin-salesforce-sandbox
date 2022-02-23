fd59fb3154bb4be8b1c9321b0fd288da79b23f12
Wyatt Roehler <Wyatt.Roehler@aunalytics.com>
2022-02-23 12:35:51 -0500

    fix(read): fixed issue reading custom queries with more result records than 2000

--- PluginSalesforce/API/Read/GetRecordsForQuery.cs
+++ PluginSalesforce/API/Read/GetRecordsForQuery.cs
@@ -35,6 +35,8 @@ namespace PluginSalesforce.API.Read
                 
                 recordsResponse =
                     JsonConvert.DeserializeObject<RecordsResponse>(await response.Content.ReadAsStringAsync());
+
+                records = recordsResponse.Records;
                 
                 foreach (var record in records)
                 {
