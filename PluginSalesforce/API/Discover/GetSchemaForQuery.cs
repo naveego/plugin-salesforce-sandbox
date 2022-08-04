3930e29b327aa3436491c8c117a30d0704061a08
Wyatt Roehler <Wyatt.Roehler@aunalytics.com>
2022-08-04 12:03:32 -0400

    refactor(discovery): updated query discovery to use shared read records

--- PluginSalesforce/API/Discover/GetSchemaForQuery.cs
+++ PluginSalesforce/API/Discover/GetSchemaForQuery.cs
@@ -18,16 +18,11 @@ namespace PluginSalesforce.API.Discover
         {
             var query = schema.Query;
 
-            // get records
-            var response = await client.GetAsync($"/query?q={HttpUtility.UrlEncode(query)}");
-            response.EnsureSuccessStatusCode();
-            
-            var recordsResponse =
-                JsonConvert.DeserializeObject<RecordsResponse>(await response.Content.ReadAsStringAsync());
+            var records = await Read.Read.GetRecordsForQuery(client, query)
+                .Take(sampleSize == 0 ? 1 : Convert.ToInt32(sampleSize))
+                .ToListAsync();
 
-            var records = recordsResponse.Records;
-
-            if (recordsResponse.TotalSize == 0)
+            if (records.Count == 0)
             {
                 return schema;
             }
